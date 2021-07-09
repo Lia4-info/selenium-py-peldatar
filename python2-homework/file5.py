d213bce027de99f9d45bee2d12dceadf422dfd3d
Lia <kornelia.timar@gmail.com>
2021-07-09 17:03:52 +0200

    file5, adat5.txt

--- python2-homework/file5.py
+++ python2-homework/file5.py
@@ -1,3 +1,3 @@
 with open("adat.txt", "r", encoding="utf8") as adat:
-    with open("adat2.txt", "w", encoding="utf8") as adat2:
-        adat2.write(adat.read())
+    with open("adat5.txt", "w", encoding="utf8") as adat5:
+        adat5.write(adat.read())
